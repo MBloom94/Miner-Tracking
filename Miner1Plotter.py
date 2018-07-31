53ef69bf15ec89884f16498b524acfda8866ae2f
MBloom94 <matthew.j.bloom@gmail.com>
2018-07-30 22:21:17 -0400

    Line graph with timestamp xaxis, still shows wrong years.

--- Miner1Plotter.py
+++ Miner1Plotter.py
@@ -1,24 +1,27 @@
 from matplotlib import pyplot as plt
 from matplotlib import animation
+from matplotlib import dates
 from matplotlib.ticker import FuncFormatter
 import time
+import datetime
 import Miner1Watcher
 
 
 # Set up figure, axis and plot element.
 fig = plt.figure()
 ax = plt.axes()
-line, = ax.plot([], [], lw = 2)
+# line, = ax.plot_date([], [], lw = 2)
+line, = ax.plot_date([], [], 'b-')
 
 # Create a new watcher.
 watcher = Miner1Watcher.Watcher()
 
 # Set watcher's stats list to the first of our list of stats.
-watcher.get_new_stats()
-# Set the x axis range to the previous uptime -> current uptime.
-ax.set_xlim([watcher.uptime[0] - 20, watcher.uptime[0]])
+# watcher.get_new_stats()
+
 # Set the y axis range from 0 to 40,000 kH/s
-ax.set_ylim([0, 40000])
+# ax.set_ylim([0, 40000])
+ax.set_ylim([0, 30000])
 
 # Styling
 fig.canvas.set_window_title('Miner1Hashrate')
@@ -26,28 +29,14 @@ fig.canvas.set_window_title('Miner1Hashrate')
 
 def megahashes(x, pos):
     '''Provide formatting for the y axis tickers.'''
-    return '{0:.3f} Mh/s'.format(x/1000)
+    return '{0:.0f} Mh/s'.format(x/1000)
 
-def hours_minutes(x, pos):
-    '''Provide formatting for the x axis tickers.'''
-    h = str(int(x//60))
-    m = str(int(x%60))
-    if len(m) < 1:
-        m = '0' + m
-    x_hm = h + ':' + m
-    return x_hm.format('{0:.0f}')
-    # return '{0:.0f}:{0:.0f}'.format(x//60, x%60)
-    # return x
-    # return time.strftime('%H:%M', time.gmtime(x // 60))
-
-# Create a formatters.
-x_formatter = FuncFormatter(hours_minutes)
+# Create formatters.
+# x_formatter = dates.DateFormatter('%H:%M:%S')
+# ax.xaxis.set_major_formatter(x_formatter)
 y_formatter = FuncFormatter(megahashes)
-ax.xaxis.set_major_formatter(x_formatter)
 ax.yaxis.set_major_formatter(y_formatter)
-ax.locator_params(axis='x', nbins=10)
-# Give a little space to the left wall.
-fig.subplots_adjust(left = 0.16)
+# ax.locator_params(axis='x', nbins=10)
 
 
 # Initialize; plot background of each frame
@@ -62,14 +51,14 @@ def animate(i):
     '''Function to drive the animation to be run each interval.'''
     # Update the stats list, allowing duplicate timestaps in minutes
     #    by default only appends new stats when stats[0][0] != stats[1][0]
-    watcher.get_new_stats(allow_dup_time = False)
-    # Print formatted newest stats in console.
+    watcher.get_new_stats()
     watcher.print_stats_pretty()
-    x = watcher.uptime  # ['minutes'] e.g. ['237']
+    x = list(dates.date2num(watcher.timestamp))  # ['timestamp'] e.g. ['']
+    # x = [dates.date2num()]
     y = watcher.hash_rate  # ['kilohashes'] e.g. ['25000']
     line.set_data(x, y)
     # Set the x axis range to the previous uptime -> current uptime.
-    ax.set_xlim([watcher.uptime[-1] - 20, watcher.uptime[-1]])
+    ax.set_xlim([x[0], x[-1]])
     return line,
 
 # Animate with the animator function
