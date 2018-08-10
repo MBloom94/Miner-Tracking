c7ebe805b07b13f49ecf5a15a257067cd5ae2631
MBloom94 <matthew.j.bloom@gmail.com>
2018-08-10 10:58:11 -0400

    add_stat now takes formatting params instead of a formatted stat.

--- Miner1Stats.py
+++ Miner1Stats.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 
 
 class Stats():
     '''Stats object to hold list of stats from Miner1's logs.'''
+    default_stat_type = 'Claymore log'
+
     def __init__(self):
         self.stats = []
 
@@ -9,16 +12,37 @@ class Stats():
     def stats_list(self):
         return self.stats
 
-    def add_stat(self, new_stat=None):
+    def add_stat(self, new_stat=None, format=True,
+                 type=None, add_timestamp=False):
         '''Append a new stat to the list of stats.'''
+
+        '''Receive a string: new_stat, bool: format,
+        string: type of log, bool: add_timestamp.
+        A new stat should always be received. Format by
+        default is true. Type is either received or is
+        set to Stats class var default_stat_type.
+        Adding a timestamp is default false but is passed
+        to the formatter either way.
+        '''
         if new_stat is None:
             print('No new stat given, none added.')
+            return False  # Allow testing if stat was added or not.
         else:
-            self.stats.append(new_stat)
+            if type is None:
+                type = self.default_stat_type
+            if format:
+                self.stats.append(self.format_stat(
+                    new_stat, type, add_timestamp))
+            else:
+                self.stats.append(new_stat)
+            return True  # Add testing if the stat was actually added correctly.
 
-    def format_csv(self, unf_stat):
+    def format_csv(self, unf_stat, add_timestamp=False):
         '''Take a string line and return it split it by ','.'''
-        return unf_stat.split(',')  # Mostly for testing purposes.
+        f_stat = unf_stat.split(',')  # Mostly for testing purposes.
+        if add_timestamp:
+            f_stat.insert(0, datetime.now())
+        return f_stat
 
     def format_claymore_log(self, unf_stat):
         '''Take an unformatted line from a Claymore log and return its items.'''
@@ -38,27 +62,37 @@ class Stats():
         '''
         return f_stat
 
-    def format_stat(self, unf_stat, add_timestamp=False, type='Claymore logs'):
+    def format_stat(self, unf_stat, type=None, add_timestamp=False):
         '''Take an unformatted string and return a list of parsed items.'''
 
-        '''Make a dict of func names to be executed depending on
+        '''Make a dict of functions to be executed depending on
         the type received. Then, return its result.
+
+        This allows for different formatters to be used based on
+        the given stat type. Makes it easier to add other log types
+        in the future, maybe from other miners than Claymore.
+        CSV is included for testing purposes.
         '''
-        source_type = {
-            'Claymore logs': self.format_claymore_log,
+        if type is None:
+            type = self.default_stat_type
+
+        stat_types = {
+            'Claymore log': self.format_claymore_log,
             'csv': self.format_csv
         }
-        formatter = source_type.get(type, lambda: 'Invalid type')
-        return formatter(unf_stat)
+        formatter = stat_types.get(type, lambda: 'Invalid type')
+        if add_timestamp:
+            return formatter(unf_stat, add_timestamp=True)
+        else:
+            return formatter(unf_stat)
 
 
 # If Miner1Stats.py is run individually...
 if __name__ == '__main__':
     stats = Stats()
-    stats.add_stat(stats.format_stat('testing,this,function', type='csv'))
+    stats.add_stat('testing,this,function', type='csv')
     print(stats.stats_list)
-    print(type(stats.stats_list[0]))
-    stats.add_stat(stats.format_stat(
-        '07:03:53:554	3654	Check and remove old log files...'))
+    stats.add_stat('testing,this,function,again', type='csv', add_timestamp=True)
     print(stats.stats_list)
-    print(type(stats.stats_list[1]))
+    stats.add_stat('07:03:53:554	3654	Check and remove old log files...')
+    print(stats.stats_list)
