31cc19a0d4a466757ad6b0b1f806ebeb73f3db57
MBloom94 <matthew.j.bloom@gmail.com>
2018-08-09 21:56:58 -0400

    Split Claymore log and append to formatted stat list.

--- Miner1Stats.py
+++ Miner1Stats.py
@@ -22,8 +22,20 @@ class Stats():
 
     def format_claymore_log(self, unf_stat):
         '''Take an unformatted line from a Claymore log and return its items.'''
-        # TODO: Parse Claymore log line.
-        f_stat = unf_stat
+
+        '''Example unf_stat:
+        07:03:53:554	3654	Check and remove old log files...
+        '''
+        f_stat = []
+        f_stat.append(unf_stat[:12])  # 07:03:53:554
+        f_stat.append(unf_stat[13:17])  # 3654
+        f_stat.append(unf_stat[18:])  # Check and... -> end of string
+
+        '''Here, the unformatted Claymore log line is split in 3.
+        f_stat[0] is the timestamp (currently string form...)
+        f_stat[1] is something... not sure yet. Messaged Claymore.
+        f_stat[2] is the message written to the log.
+        '''
         return f_stat
 
     def format_stat(self, unf_stat, add_timestamp=False, type='Claymore logs'):
@@ -40,13 +52,13 @@ class Stats():
         return formatter(unf_stat)
 
 
-
 # If Miner1Stats.py is run individually...
 if __name__ == '__main__':
     stats = Stats()
     stats.add_stat(stats.format_stat('testing,this,function', type='csv'))
     print(stats.stats_list)
     print(type(stats.stats_list[0]))
-    stats.add_stat(stats.format_stat('07:03:53:554	3654	Check and remove old log files...'))
+    stats.add_stat(stats.format_stat(
+        '07:03:53:554	3654	Check and remove old log files...'))
     print(stats.stats_list)
     print(type(stats.stats_list[1]))
