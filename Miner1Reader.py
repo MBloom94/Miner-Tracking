09a52069980f6c4aa634d43df5c24f2623acbe62
MBloom94 <matthew.j.bloom@gmail.com>
2018-08-14 19:06:51 -0400

    Create stats obj, read Claymore logs, add stats to obj & print.

--- Miner1Reader.py
+++ Miner1Reader.py
@@ -1,20 +1,36 @@
+import Miner1Stats as Stats
+import Miner1Watcher as Watcher
 
 
 class Reader():
     '''Read, parse, and save a log from Claymore's Miner.'''
+    default_file_name = 'sample logs/1533940877_log.txt'
 
     def __init__(self, file_name=None):
         '''Initialize Reader, prep file for reading.'''
         if file_name is None:
-            self.file_name = 'sample logs/1533121433_log.txt'
+            self.file_name = self.default_file_name
         else:
             self.file_name = file_name
-        self.f = open(self.file_name, 'r')
-        self.line = ''  # Get temp 'line' string ready for use.
+        # Create stats object for Claymore log files.
+        self.stats = Stats.Stats(type='Claymore log')
+        # Open file to read, for each line add it to stats.
+        # add_stat will format it as a Claymore log and add data
+        # to a hash rates list.
+        with open(self.file_name, 'r') as f:
+            for f_line in f:
+                f_line = f.readline()
+                if f_line.strip():
+                    self.stats.add_stat(f_line)
+                    # print(f_line, end='')
+
+        print(self.stats.hash_rates)
+
+    # def print_hash_rate(self):
+    #     Watcher.print_stats_pretty(self.stats.hash_rates)
 
 
 # If Miner1Reader.py is run individually...
 if __name__ == '__main__':
     reader = Reader()
-
-    reader.f.close()
+    # reader.print_hash_rate()
