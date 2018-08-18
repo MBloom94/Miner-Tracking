f5b68bb2919d6ee318bb62bf777a257dc2f5b64b
MBloom94 <matthew.j.bloom@gmail.com>
2018-08-17 20:59:02 -0400

    Plot live using Watcher testing.

--- Main.py
+++ Main.py
@@ -3,10 +3,7 @@ import Miner1Reader
 import Miner1Watcher
 import Miner1Plotter
 
-csv_stats = Miner1Stats.Stats('csv')
-csv_stats.add_stat('testing,this,function')
-print(csv_stats.stats_list)
-csv_stats.add_stat('testing,this,function,again', add_timestamp=True)
-print(csv_stats.stats_list)
-clay_stats = Miner1Stats.Stats('Claymore log')
-clay_stats.add_stat('07:03:53:554	3654	Check and remove old log files...')
+watcher = Miner1Watcher.Watcher()
+plotter = Miner1Plotter.Plotter()
+
+plotter.plot_live(watcher)
