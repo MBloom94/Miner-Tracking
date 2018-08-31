5129e65e25a580a42cd88c928a5fb2575e8441e2
MBloom94 <matthew.j.bloom@gmail.com>
2018-08-31 18:48:46 -0400

    Auto read newest log file in path directory.

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import Miner1Stats
 import Miner1Reader
 import Miner1Watcher
@@ -18,27 +19,34 @@ args = parser.parse_args()
 if args.path:
     path = args.path
 else:
-    path = "C:\Claymore\Claymore Miner\Claymore's Dual Ethereum+Decred_Siacoin_Lbry_Pascal_Blake2s_Keccak AMD+NVIDIA GPU Miner v11.8/"
+    path = ("C:/Claymore/Claymore Miner/"
+            "Claymore's Dual Ethereum"
+            "+Decred_Siacoin_Lbry_Pascal_Blake2s_Keccak "
+            "AMD+NVIDIA GPU Miner v11.8/")
 
 if args.file:
     f = args.file
 else:
-    f = "1534729180_log.txt"
+    # Get most recent log in path.
+    # Iterate files in order and the last one stays assigned to f
+    for file in os.listdir(path):
+        if file.endswith('_log.txt'):
+            f = file
 
 if args.interval:
     inter = args.interval
 else:
     inter = 60
 
-watcher = Miner1Watcher.Watcher()
-reader = Miner1Reader.Reader(path, f)
 plotter = Miner1Plotter.Plotter(inter)
+reader = Miner1Reader.Reader(path, f)
 
 # Plot Static or Live
 if args.live:
     if args.read_log:
         plotter.plot_live(reader)
     else:
+        watcher = Miner1Watcher.Watcher()
         plotter.plot_live(watcher)
 else:
     plotter.plot_static(reader)
